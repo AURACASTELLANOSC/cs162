1c1c14a1e9860d0602923b437f0906aa18790ee4
AURACASTELLANOSC <90876584+AURACASTELLANOSC@users.noreply.github.com>
2022-09-06 21:44:39 -0700

    Add files via upload

---
+++ hw3v1/list.cpp
@@ -0,0 +1,106 @@
+#include "list.h"
+
+using namespace std;
+
+//default constructor
+list::list()
+{
+  cout << "call default list"  << endl;
+  size = 0;
+  index  = nullptr;
+}
+
+//destructor
+list::~list()
+{
+   if(index)
+   {
+      delete []  index;
+      index = nullptr;
+   }
+}
+//assignment operator
+list& list::operator=(const list& source)
+{
+   if(this != &source)
+   {
+      if(index)
+      {
+         delete []index;
+         index = nullptr;
+      }
+      index = new word[source.size];
+      this->index = source.index;
+      this->size = source.size;
+   }
+   return *this;
+}
+//copy constructor
+list::list(const list&source)
+{
+      index = new word[source.size];    
+      this->index = source.index;
+      this->size = source.size;
+}
+//insert the new word
+void list::insert(char * newWord)
+{ 
+   bool existWord = false;
+   char * tempWord = nullptr; 
+  
+   for (int i = 0; i < size; i++)
+   {
+      tempWord = new char[index[i].getLength() + 2]{0};
+      //Get data value from word instance in index[i]
+      index[i].getData(tempWord);
+      //If word exist then
+      if(strcmp(tempWord, newWord) == 0){ 
+        //Set count value 
+         index[i].setCount();
+         existWord = true;
+         delete []tempWord;
+	 tempWord = nullptr;
+         break;
+      }
+     //Delete memory used by tempWord array
+      delete []tempWord;
+      tempWord = nullptr;
+   }
+
+   if(!existWord){
+      //Re-size array
+      //Memory size + 1 (space for element to be inserted)
+      word *temp = new word[size+1];  
+      //If size > zero then copy elementos of index 
+      if(size > 0){
+         for (int i = 0; i < (size); i++) {
+           temp[i] = index[i];
+         }
+         //Delete memory of index array
+         delete[] index;
+	 index = nullptr;
+      }
+      //assign value of temp array to index array
+      index = temp;
+      temp = nullptr;
+      //Create a new word instance
+      word * myWord = new word();
+      //Set value data in instance of word
+      myWord->setData(newWord);
+      //Increment word count (initial value = 1)
+      myWord->setCount();
+      //Insert word instance in the reserved space 
+      index[size++] = *myWord;   
+      delete myWord;
+      myWord = nullptr;
+   }   
+}
+
+void list::print()
+{
+   for(int i = 0; i < size; i++)
+   {
+      index[i].printWord();
+      cout << endl;
+   }
+}
---
+++ hw3v1/list.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "word.h"
+#include <fstream>
+#include <iostream>
+#include <cstring>
+#include <string.h>
+using namespace std;
+
+class list
+{
+      public:
+      //default constructor
+      list();
+      //destructor
+      ~list();
+      //copy constructor
+      list(const list&);
+      //copy assignment operator
+      list& operator=(const list&);
+      //may in constr,acces, mutat, and helpe
+      //insert() insert words into the index array
+      void insert( char *newWord); 
+      //print() display contents of the index aray
+      void print(); 
+      private:
+      word *index; //dynamic array of word objects
+      int size; //num of elements in the array
+};
